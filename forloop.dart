1e911ae92ee183758355b4626916184c4f4902cb
Michael omofemiwa <mychelomofem004@outlook.com>
2024-06-11 23:11:55 +0100

    dartcode 3

--- forloop.dart
+++ forloop.dart
@@ -1,7 +1,7 @@
 void main() {
   for (int i = 0; i <= 10; i++) {
-    print(i);
-    // find even numbers between 1 to ten
+    //print(i);
+    // find EVEN numbers between 1 to ten
     //if (i % 2 == 0) {
       //print(i);
     //};
@@ -14,6 +14,22 @@ List planetList =["mercury", "mars", "earth", "venus"];
 for (String planet in planetList){
   //print(planet);
 }
-  
+  // SINGLE Multiplication
+  int mult = 2;
+  for (int i = 1; i <= 12; i++){
+    //print('$mult * $i = ${mult * i}');
+  }
+  // CONTINUE KEYWORD
+  for (int i = 1; i <= 10; i++){
+    // neglect 5 and continue
+    //if (i == 5){
+     // continue;
+    //}
+    // PRINT THE OPPOSITE OF EVEN NUMBERS(ODD NUMBERS)
+    if (i % 2 == 0){
+      continue;
+    }
+    print(i);
+  }
   
 }
--- nestedforloop.dart
+++ nestedforloop.dart
@@ -1,10 +1,19 @@
-void main(){
-  for (int i = 1; i <= 3; i++) {
+void main() {
+  for (int i = 1; i <= 12; i++) {
     
     for(int j = 1; j <= 3; j++) {
-      print("$i $j");
-      if (i==2 && j==2){
-        break;
+      //print("$i $j"); NESTED FOR LOOP
+      // MULTIPLICATION OF LARGE NUMBERS
+      for(int j = 1; j <= 12; j++) {
+      //print("$i * $j = ${i * j}");
+      //BREAK KEYWORD
+      //if (i==2 && j==2){
+        //break;
+      // CONTINUE KEYWORD IN NESTED LOOP
+      if(i==1 && j==4){
+        continue;
+      }
+      print("$i * $j = ${i * j}");
       }
 
     }
