c080fb547440b84c31742610e0e2bc48901d0c9e
Michael omofemiwa <mychelomofem004@outlook.com>
2024-06-12 18:39:30 +0100

    dartcode 5(CUSTOM EXCEPTION 1)

---
+++ exceptnhandn.dart
@@ -0,0 +1,59 @@
+import 'dart:async';
+
+void main(){
+  //CASE 1: USE "ON" CLAUSE TO HANDLE IT : WHEN YOU KNOW THE EXCEPTION
+//INTEGERDIVISIONBYZEROEXCEPTION
+  try{
+  int result = 12 ~/ 0;
+  print(result);
+} on IntegerDivisionByZeroException {
+  print("cannot be divided by zero");
+  print("");
+}
+
+// CASE 2: USE "CATCH" CLAUSE TO HANDLE IT : WHEN YOU DONT KNOW THE EXCEPTION
+  try{
+  int result = 12 ~/ 0;
+  print(result);
+} catch (e) {
+  print("the exception thrown is $e");
+  print("");
+}
+// CASE 3: CATCH clause with Exception Object and StackTrace object
+// USING STACK TRACE to know the events that occured before exception was thrown
+try{
+  int result = 12 ~/ 0;
+  print(result);
+} catch (e, s) {
+  print("the exception thrown is $e");
+  print("Stack Trace \n $s");
+}
+
+// FINALLY CLAUSE
+ try{
+  int result = 12 ~/ 0;
+  print(result);
+} catch (e) {
+  print("the exception thrown is $e");
+  print("");
+} finally{
+  print("this is a FINALLY CLAUSE and its always executed");
+  
+}
+
+print(""); print("case 5");
+// CUSTOM EXCEPTION
+depositMoney(-200);
+}
+
+class DepositException implements Exception {
+errorMessage(){
+    print("You cannot enter amount less than 0");
+  }
+}
+
+void depositMoney(int amount){
+  if(amount < 0){
+    throw new DepositException();
+  }
+}
--- functparamtypes.dart
+++ functparamtypes.dart
@@ -2,33 +2,41 @@
 //REQUIRED PARAMETERS if we put just two parameter values in the function call in
 // main function, it will give error. so 3 param values is required..a MUST!!!!
 void main(){
-printCities("new orleans", "barcelona", "arsenal");
-print("");
+//printCities("new orleans", "barcelona", "arsenal");
+//print("");
 
-printCountries("NIGERIA", "SWEDEN", "USA");
-print("");
+//printCountries("NIGERIA", "SWEDEN", "USA");
+//print("");
 
-findVolume(5, 9, 8);
+findVolume(5, breadth: 6, height: 4);
 
 }
-void printCities(String name1, String name2, String name3){
-  print("name 1 is $name1");
-  print("name 1 is $name2");
-  print("name 1 is $name3");
-}
+//void printCities(String name1, String name2, String name3){
+ // print("name 1 is $name1");
+ // print("name 1 is $name2");
+ // print("name 1 is $name3");
+//}
 
 // OPTIONAL POSITIONAL PARAMETERS USING []
-void printCountries(String name1, String name2, [String name3]) { // its supposed to work, but not working.
-  print("name 1 is $name1");
-  print("name 1 is $name2");
-  print("name 1 is $name3");
-}
+//void printCountries(String name1, String name2, [String name3]) { // its supposed to work, but not working.
+  //print("name 1 is $name1");
+ // print("name 1 is $name2");
+ // print("name 1 is $name3");
+//}
 
 //OPTIONAL NAMED PARAMETERS USING {}
-int findVolume(int length,{int breadth, int height}) { // its supposed to work, but not working.
+//void findVolume(int length,{int breadth, int height}) { // its supposed to work, but not working.
+  //print("name 1 is $length");
+ // print("name 1 is $breadth");
+  //print("name 3 is $height");  
+ // print("volume is ${length*breadth*height}");
+//}
+
+//OPTIONAL DEFAULT PARAMETERS USING {}
+void findVolume(int length,{int breadth=10 , int height=15 }) { // its supposed to work, but not working.
   print("name 1 is $length");
   print("name 1 is $breadth");
-  print("name 1 is $height");
-
-  return length * breadth * height;
+  print("name 3 is $height");  
+  print("volume is ${length*breadth*height}");
 }
+ 
